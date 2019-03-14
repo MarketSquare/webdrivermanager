f432065f4552ea759ca0e03e573385a993dba34b
Dmitry Misharov <dmisharo@redhat.com>
2019-03-14 09:06:41 +0100

    Fixed imports

--- webdrivermanager/__init__.py
+++ webdrivermanager/__init__.py
@@ -1,8 +1,5 @@
 # -*- coding: utf-8 -*-
 
-try:
-    from webdrivermanager import WebDriverManagerBase, ChromeDriverManager, GeckoDriverManager, OperaChromiumDriverManager, EdgeDriverManager, AVAILABLE_DRIVERS
-except ImportError:
-    from .webdrivermanager import WebDriverManagerBase, ChromeDriverManager, GeckoDriverManager, OperaChromiumDriverManager, EdgeDriverManager, AVAILABLE_DRIVERS
+from .webdrivermanager import WebDriverManagerBase, ChromeDriverManager, GeckoDriverManager, OperaChromiumDriverManager, EdgeDriverManager, AVAILABLE_DRIVERS
 
 __all__ = ['WebDriverManagerBase', 'ChromeDriverManager', 'GeckoDriverManager', 'OperaChromiumDriverManager', 'EdgeDriverManager', 'AVAILABLE_DRIVERS']
