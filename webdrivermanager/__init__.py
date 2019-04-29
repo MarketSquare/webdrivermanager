0cbeec71128302f562c1145c804ab861a6640584
Jani Mikkonen <jani.mikkonen@siili.com>
2019-04-29 15:12:19 +0300

    Basic code for Internet Explorer

--- webdrivermanager/__init__.py
+++ webdrivermanager/__init__.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 
-from .webdrivermanager import WebDriverManagerBase, ChromeDriverManager, GeckoDriverManager, OperaChromiumDriverManager, EdgeDriverManager, AVAILABLE_DRIVERS
+from .webdrivermanager import WebDriverManagerBase, ChromeDriverManager, GeckoDriverManager, OperaChromiumDriverManager, EdgeDriverManager, IEDriverManager, AVAILABLE_DRIVERS
 
 from .version import get_version
-__all__ = ['WebDriverManagerBase', 'ChromeDriverManager', 'GeckoDriverManager', 'OperaChromiumDriverManager', 'EdgeDriverManager', 'AVAILABLE_DRIVERS', 'get_version']
+
+__all__ = ['WebDriverManagerBase', 'ChromeDriverManager', 'GeckoDriverManager', 'OperaChromiumDriverManager', 'EdgeDriverManager', 'IEDriverManager', 'get_version', AVAILABLE_DRIVERS]
--- webdrivermanager/cli.py
+++ webdrivermanager/cli.py
@@ -5,8 +5,15 @@ import sys
 import os.path
 import argparse
 from requests import ConnectionError
-from .version import get_version
-from .webdrivermanager import AVAILABLE_DRIVERS as DOWNLOADERS
+
+try:
+    from .version import get_version
+except ImportError:
+    from version import get_version
+try:
+    from .webdrivermanager import AVAILABLE_DRIVERS as DOWNLOADERS
+except ImportError:
+    from webdrivermanager import AVAILABLE_DRIVERS as DOWNLOADERS
 
 
 OS_NAMES = ['mac', 'win', 'linux']
