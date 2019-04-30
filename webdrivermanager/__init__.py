17bf74534d032941fe7946f5ab27b658be572616
Jani Mikkonen <jani.mikkonen@siili.com>
2019-04-30 12:59:19 +0300

    Bugfix: AVAILABLE_DRIVERS exported incorrectly.
    !minor

--- webdrivermanager/__init__.py
+++ webdrivermanager/__init__.py
@@ -4,4 +4,4 @@ from .webdrivermanager import WebDriverManagerBase, ChromeDriverManager, GeckoDr
 
 from .version import get_version
 
-__all__ = ['WebDriverManagerBase', 'ChromeDriverManager', 'GeckoDriverManager', 'OperaChromiumDriverManager', 'EdgeDriverManager', 'IEDriverManager', 'get_version', AVAILABLE_DRIVERS]
+__all__ = ['WebDriverManagerBase', 'ChromeDriverManager', 'GeckoDriverManager', 'OperaChromiumDriverManager', 'EdgeDriverManager', 'IEDriverManager', 'get_version', 'AVAILABLE_DRIVERS']
