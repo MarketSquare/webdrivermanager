f4bdd557db00e29c45bf7adf7c569cfa212384e6
Jani Mikkonen <jani.mikkonen@gmail.com>
2021-01-24 20:17:22 +0200

    Reflect supported pythons in setup.py. !nocl

--- setup.py
+++ setup.py
@@ -28,10 +28,10 @@ Intended Audience :: Information Technology
 Intended Audience :: System Administrators
 License :: OSI Approved :: MIT License
 Programming Language :: Python
-Programming Language :: Python :: 2
-Programming Language :: Python :: 2.7
 Programming Language :: Python :: 3
 Programming Language :: Python :: 3.7
+Programming Language :: Python :: 3.8
+Programming Language :: Python :: 3.9
 Topic :: Software Development :: Libraries
 Topic :: Software Development :: Quality Assurance
 Topic :: Software Development :: Testing
